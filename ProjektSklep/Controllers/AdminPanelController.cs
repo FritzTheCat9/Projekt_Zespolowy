06c13b2a66bb0cf8ee1ae85c516afe53eb068d62
Bartłomiej Umiński <bartlomiejuminski1999@gmail.com>
2021-01-09 15:01:55 +0100

    ukrywanie produktów i kategorii z panelu admina

--- ProjektSklep/Controllers/AdminPanelController.cs
+++ ProjektSklep/Controllers/AdminPanelController.cs
@@ -1,5 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using ProjektSklep.Data;
+using ProjektSklep.Models.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -9,6 +13,14 @@ namespace ProjektSklep.Models
 {
     public class AdminPanelController : Controller
     {
+        private readonly ShopContext _context;
+        private readonly ILogger<AdminPanelController> _logger;
+        public AdminPanelController(ILogger<AdminPanelController> logger, ShopContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
+
         [Authorize(Roles = "Administrator")]
         public IActionResult Index()
         {
